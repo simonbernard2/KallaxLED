171e645795ca1f9d3fb71f59f41ccdd4472c095a
Simon Bernard <simonbernard@gmail.com>
2025-11-03 17:26:22 -0500

    Changed fade_in to transition
      - Perfect fade into current_color to desired color

--- app/strip.py
+++ app/strip.py
@@ -1,6 +1,10 @@
 import board
 import neopixel
 import math
+import time
+from typing import Any
+
+RGB = tuple[int, int, int]
 
 
 class Strip:
@@ -15,19 +19,28 @@ class Strip:
         self.pixels.fill((0, 0, 0))
         self.pixels.show()
 
-    def fade_in(
-        self, led_index: int, color: tuple[int, int, int], speed_ms: int = 1000
-    ) -> None:
+    def transition(self, led_index: int, color: RGB, speed_ms: int = 1000) -> None:
         end_r, end_g, end_b = color
-        current_r, current_g, current_b = [0, 0, 0]
-        fade_r = math.ceil(end_r / speed_ms)
-        fade_g = math.ceil(end_g / speed_ms)
-        fade_b = math.ceil(end_b / speed_ms)
-        print(fade_r)
+        current_r, current_g, current_b = self.pixels[led_index]
+
+        step_r, min_max_r = self._fade_step(current_r, end_r, speed_ms)
+        step_g, min_max_g = self._fade_step(current_g, end_g, speed_ms)
+        step_b, min_max_b = self._fade_step(current_b, end_b, speed_ms)
         while not (end_r == current_r and end_g == current_g and end_b == current_b):
-            current_r = min(current_r + fade_r, end_r)
-            current_g = min(current_g + fade_g, end_g)
-            current_b = min(current_b + fade_b, end_b)
+            current_r = min_max_r(current_r + step_r, end_r)
+            current_g = min_max_g(current_g + step_g, end_g)
+            current_b = min_max_b(current_b + step_b, end_b)
 
             self.pixels[led_index] = (current_r, current_g, current_b)
             self.pixels.show()
+            time.sleep(speed_ms / (250 * 1000))
+
+    def _fade_step(
+        self, start_value: int, end_value: int, speed_ms: int
+    ) -> tuple[int, Any]:
+        diff = end_value - start_value
+        step = diff / speed_ms
+        if diff >= 0:
+            return (math.ceil(step), min)
+        else:
+            return (math.floor(step), max)
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import time
 if __name__ == "__main__":
     strip = Strip.default()
     try:
-        strip.fade_in(0, (255, 0, 0))
-        time.sleep(1)
+        # strip.transition(0, (0, 0, 0))
+        while True:
+            strip.transition(0, (255, 0, 0), 10)
+            strip.transition(0, (0, 0, 100), 20)
+            strip.transition(0, (0, 100, 0), 30)
 
     finally:
         strip.turn_off()
