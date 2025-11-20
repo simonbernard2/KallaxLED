7060407d5bc32c0a37d29350265e63c3148b7d73
Simon Bernard <simonbernard@gmail.com>
2025-11-20 12:04:58 -0500

    color picker updates LEDs

--- frontend/app/features/bookshelf/components/box.tsx
+++ frontend/app/features/bookshelf/components/box.tsx
@@ -2,32 +2,41 @@ import { rgbToCSS, isTurnedOff } from "~/utils/utils";
 import type { BoxType } from "../types/bookshelfTypes";
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "~/store";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mutateBoxColor } from "../slices/bookshelfSlice";
 
 const Box = (props: BoxType) => {
   const dispatch = useDispatch();
-  const color = isTurnedOff(props.rgb) ? 'transparent' : rgbToCSS(props.rgb)
-  const [backgroundColor, setBackgroundColor] = useState(color)
+  const [currentRgb, setCurrentRgb] = useState(props.rgb)
+  const backgroundColor = isTurnedOff(currentRgb) ? 'transparent' : rgbToCSS(currentRgb)
   const { selectedColor } = useSelector((state: RootState) => state.colorpicker)
 
+  useEffect(() => {
+    setCurrentRgb(props.rgb)
+  }, [props.rgb])
+
   let className = "flex h-32 w-32 p-2 border-2"
-  if (isTurnedOff(props.rgb)) className += " border-dashed "
+  if (isTurnedOff(currentRgb)) className += " border-dashed "
   if (selectedColor) className += " cursor-pointer "
 
-  const handleOnClick = () => {
+  const handleOnClick = async () => {
     if (!selectedColor) return
 
     const box: BoxType = {
       id: props.id,
       rgb: selectedColor.rgb
     }
-    setBackgroundColor(rgbToCSS(selectedColor.rgb))
+    setCurrentRgb(selectedColor.rgb)
     dispatch(mutateBoxColor(box))
+    await fetch(`http://192.168.17.39:5000/update_led/${box.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(box)
+    })
   }
   return (
     <div onClick={handleOnClick} className={className} style={{ backgroundColor: backgroundColor }}>
-      {isTurnedOff(props.rgb) && <span>OFF</span>}
+      {isTurnedOff(currentRgb) && <span>OFF</span>}
     </div>
   )
 }
