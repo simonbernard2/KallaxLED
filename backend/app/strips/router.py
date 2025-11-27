405c647112938cafb18c7dd580a0d5eeacf92727
Simon Bernard <simonbernard@gmail.com>
2025-11-27 17:00:13 -0500

    update leds endpoint to turn off other leds for config

--- backend/app/strips/router.py
+++ backend/app/strips/router.py
@@ -10,5 +10,6 @@ router = APIRouter()
 async def update_led(
     led_id: int, color: models.Color, led_strip: deps.LedStripDep
 ) -> models.LED:
+    led_strip.turn_off()
     led_strip.update_led(led_id, color)
     return models.LED(id=led_id, rgb=color.rgb)
