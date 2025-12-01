c52810b181dcae0c393a873d96cde29502b5fa64
mazine <mazmrini@hotmail.com>
2025-11-30 19:01:00 -0500

    Add animations module

---
+++ backend/app/animations/mappers.py
@@ -0,0 +1,24 @@
+from app.strips.models import StripAnimation, StripAnimationStep, LED
+from app.grids.models import Grid
+import app.animations.models as models
+
+
+def to_strip_animation(grid: Grid, animation: models.Animation) -> StripAnimation:
+    steps = [to_strip_step(grid, s) for s in animation.steps]
+    return StripAnimation(steps=steps)
+
+
+def to_strip_step(grid: Grid, step: models.AnimationStep) -> StripAnimationStep:
+    leds = []
+    for e in step.events:
+        leds += to_leds(grid, e)
+
+    return StripAnimationStep(
+        leds=leds,
+        delay_ms=step.delay_ms
+    )
+
+def to_leds(grid: Grid, e: models.BoxEvent) -> list[LED]:
+    box_leds = grid.boxes[e.i][e.j].leds
+
+    return [LED(id=led.id, rgb=e.rgb) for led in box_leds]
--- backend/app/strips/stub/neopixel.py
+++ backend/app/strips/stub/neopixel.py
@@ -1,3 +1,26 @@
 class NeoPixel:
-    def __init__(self, *args, **kwargs) -> None:
-        self.pixels: any = []
+    def __init__(self, g_pid, number_of_leds, **kwargs) -> None:
+        self.pixels: any = [(0, 0, 0) for i in range(number_of_leds)]
+
+    def __getitem__(self, key: int) -> any:
+        return self.pixels[key]
+
+    def __setitem__(self, key: int, value: tuple[int, int, int]) -> None:
+        self.pixels[key] = value
+
+    def show(self) -> None:
+        strip = ""
+        for p in self.pixels:
+            if NeoPixel.__is_off(p):
+                strip += "."
+            else:
+                strip += "o"
+        
+        print(strip)
+
+    def fill(self, rgb: tuple[int, int, int]) -> None:
+        self.pixels = [rgb for _ in self.pixels]
+
+
+    def __is_off(pixel: tuple[int, int, int]) -> bool:
+        return pixel == (0, 0, 0)
