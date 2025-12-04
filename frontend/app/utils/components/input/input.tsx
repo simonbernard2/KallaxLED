0131c34463fdd7ddcfee748d484f1cfb9b2ebdbf
Simon Bernard <simonbernard@gmail.com>
2025-12-04 11:24:29 -0500

    Fix Input styling

--- frontend/app/utils/components/input/input.tsx
+++ frontend/app/utils/components/input/input.tsx
@@ -13,8 +13,8 @@ const Input = (props: Props) => {
   const { name, label, type, min, max, value, onChange } = props
   return (
     <div className="flex flex-col">
-      <label htmlFor={name}>{label}:</label>
-      <input className="bg-neutral-700 px-2 py-1 rounded" type={type} value={value} min={min} max={max} onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e)} />
+      <label htmlFor={name} className="font-semibold">{label}:</label>
+      <input className="bg-neutral-300 dark:bg-neutral-700 focus:outline-neutral-500 px-2 py-1 rounded" type={type} value={value} min={min} max={max} onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e)} />
 
     </div>
   )
